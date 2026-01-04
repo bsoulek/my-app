57d67ad992392d514bf8121f92277b117e0e8911
bsoulek <brent.soulek@gmail.com>
2026-01-04 06:12:01 +0000

    angular 19 upgrade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button'
-import { MatCardModule } from '@angular/material/card'
-
-import { HomeComponent } from './home/home.component';
-import { PlaygroundComponent } from "./playground/playground.component";
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet,
-    MatButtonModule,
-    MatCardModule,
-    HomeComponent,
-    PlaygroundComponent
+    RouterOutlet
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
