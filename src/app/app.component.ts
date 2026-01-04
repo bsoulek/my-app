28f1ee7d1f0a7ad5d3b09447c6e72e76776bfe5f
bsoulek <brent.soulek@gmail.com>
2026-01-04 05:00:27 +0000

    application shell

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Angular';
+  title = 'Angular Sandbox';
 }
